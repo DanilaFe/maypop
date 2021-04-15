4d507351fe4c4af1849c884c172321f43477404d
Danila Fedorin <danila.fedorin@gmail.com>
2021-04-15 02:31:59 -0700

    Generate summaries into markdown files.

--- script/pages.sh
+++ script/pages.sh
@@ -4,6 +4,7 @@ for file in $(find . -name "*.lhs"); do
     echo "---" > $DEST
     echo "title: Module \"$BASENAME\"" >> $DEST
     echo "date: $(git log -1 --pretty="format:%ci" $file)" >> $DEST
+    echo "summary: $(cat desc/$BASENAME.txt)" >> $DEST
     echo "---" >> $DEST
     ${0%/*}/markdown.sh $file >> $DEST
 done
