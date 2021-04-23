1a210417edb1284cfb3fd7070f669f7f9afaf87c
Danila Fedorin <danila.fedorin@gmail.com>
2021-04-23 01:22:20 -0700

    Add extra output to pages.

--- script/pages.sh
+++ script/pages.sh
@@ -6,6 +6,10 @@ for file in $(find . -name "*.lhs"); do
     echo "title: Module \"$BASENAME\"" >> $DEST
     echo "date: $(git log -1 --pretty="format:%ci" $file)" >> $DEST
     echo "summary: $(cat desc/$BASENAME.txt)" >> $DEST
+    echo "shorthash: $(git log --pretty=format:'%h' -n 1 $file)" >> $DEST
+    echo "hash: $(git log --pretty=format:'%H' -n 1 $file)" >> $DEST
+    echo "date: $(git log --pretty=format:'%aI' -n 1 $file)" >> $DEST
+    echo "subject: $(git log --pretty=format:'%s' -n 1 $file)" >> $DEST
     echo "---" >> $DEST
     ${0%/*}/markdown.sh $file >> $DEST
 done
