2954fd6dd9fafe6d808c823900cd53ed98e7222d
Danila Fedorin <danila.fedorin@gmail.com>
2021-04-22 20:30:01 -0700

    Move hugo files into Maypop directory.

--- script/pages.sh
+++ script/pages.sh
@@ -1,6 +1,7 @@
+mkdir -p content/modules
 for file in $(find . -name "*.lhs"); do
     BASENAME=$(basename $file ".lhs")
-    DEST=$1/content/modules/$BASENAME.md
+    DEST=content/modules/$BASENAME.md
     echo "---" > $DEST
     echo "title: Module \"$BASENAME\"" >> $DEST
     echo "date: $(git log -1 --pretty="format:%ci" $file)" >> $DEST
